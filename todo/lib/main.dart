64976445cb6dc3e54e4f12110016a9f8893b02ff
Ruan <ruan4f@gmail.com>
2020-03-01 14:00:27 -0300

    Save data in shared preferences

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo/models/item.dart';
 
 void main() => runApp(App());
@@ -20,9 +23,9 @@ class HomePage extends StatefulWidget {
 
   HomePage() {
     items = [];
-    items.add(Item(title: 'Item 1', done: false));
-    items.add(Item(title: 'Item 2', done: true));
-    items.add(Item(title: 'Item 3', done: false));
+    // items.add(Item(title: 'Item 1', done: false));
+    // items.add(Item(title: 'Item 2', done: true));
+    // items.add(Item(title: 'Item 3', done: false));
   }
 
   @override
@@ -42,13 +45,37 @@ class _HomePageState extends State<HomePage> {
       ));
       //newTaskCtrl.text = '';
       newTaskCtrl.clear();
-    });
+      save();
+    });    
   }
 
   void remove(int index) {
     setState(() {
       widget.items.removeAt(index);
+      save();
     });
+  }
+
+  Future load() async {
+    var prefs = await SharedPreferences.getInstance();
+    var data = prefs.getString('data');
+
+    if (data != null) {
+      Iterable decoded = jsonDecode(data);
+      List<Item> result = decoded.map((x) => Item.fromJson(x)).toList();
+      setState(() {
+        widget.items = result;
+      });
+    }
+  }
+
+  save() async {
+    var prefs = await SharedPreferences.getInstance();
+    await prefs.setString('data', jsonEncode(widget.items));
+  }
+
+  _HomePageState() {
+    load();
   }
 
   @override
@@ -81,6 +108,7 @@ class _HomePageState extends State<HomePage> {
               onChanged: (value) {
                 setState(() {
                   item.done = value;
+                  save();
                 });
               },
             ),
