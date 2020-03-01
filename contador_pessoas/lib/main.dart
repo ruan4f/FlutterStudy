7bad2037bece57de8d6986ecfa59188aba9ecc98
Ruan <ruan4f@gmail.com>
2020-03-01 14:35:10 -0300

    New image

--- contador_pessoas/lib/main.dart
+++ contador_pessoas/lib/main.dart
@@ -3,8 +3,56 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: 'Contato de Pessoas',
-    home: Container(
-      color: Colors.white,
+    home: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          'Pessoas: 0',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: FlatButton(
+                child: Text(
+                  '+1',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(10.0),
+              child: FlatButton(
+                child: Text(
+                  '-1',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: () {},
+              ),
+            ),
+          ],
+        ),
+        Text(
+          'Pode Entrar |',
+          style: TextStyle(
+            color: Colors.white,
+            fontStyle: FontStyle.italic,
+            fontSize: 30.0,
+          ),
+        ),
+      ],
     ),
   ));
 }
