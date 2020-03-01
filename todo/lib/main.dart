019d229c20fefa63d1fe2cdae7e4253af88fb3c9
Ruan <ruan4f@gmail.com>
2020-03-01 12:58:26 -0300

    Update main.dart

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/models/item.dart';
 
 void main() => runApp(App());
 
@@ -14,15 +15,32 @@ class App extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+  
+  HomePage(){
+    items = [];
+    items.add(Item(title: 'Item 1', done: false));
+    items.add(Item(title: 'Item 2', done: true));
+    items.add(Item(title: 'Item 3', done: false));
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Todo App"),
       ),
-      body: Container(
-        child: Center(child: Text("Olá Mundo")),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder:(BuildContext ctxt, int index) {
+          return Text(widget.items[index].title);
+        } ,
       ),
     );
   }
