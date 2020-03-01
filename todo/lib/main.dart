5c0cc7c1f336ad6cac25def9d1316e295fa48512
Ruan <ruan4f@gmail.com>
2020-03-01 13:13:05 -0300

    New task controller

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -30,11 +30,25 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var newTaskCtrl = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Todo App"),
+        title: TextFormField(
+          controller: newTaskCtrl,
+          keyboardType: TextInputType.text,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+          ),
+          decoration: InputDecoration(
+              labelText: 'Nova Tarefa',
+              labelStyle: TextStyle(
+                color: Colors.white,
+              )),
+        ),
       ),
       body: ListView.builder(
         itemCount: widget.items.length,
