09cf9fd77c67d10195168b75057185ca17e5e467
Ruan <ruan4f@gmail.com>
2020-03-02 23:36:23 -0300

    Connect to API

--- conversor_moeda/lib/main.dart
+++ conversor_moeda/lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:async/async.dart';
+import 'dart:convert';
 
 const request = 'https://api.hgbrasil.com/finance?key=50777088';
 
-void main() => runApp(
-      MaterialApp(
-        home: Container(),
-      ),
-    );
+void main() async {
+  http.Response response = await http.get(request);
+  print(json.decode(response.body)['results']['currencies']);
+
+  runApp(MaterialApp(
+    home: Container(),
+  ));
+}
