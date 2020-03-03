13af43d56cc79945a5b917f3696ed769f9ece190
Ruan <ruan4f@gmail.com>
2020-03-02 23:40:14 -0300

    Update main.dart

--- conversor_moeda/lib/main.dart
+++ conversor_moeda/lib/main.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const request = 'https://api.hgbrasil.com/finance?key=50777088';
 
 void main() async {
-  http.Response response = await http.get(request);
-  print(json.decode(response.body)['results']['currencies']);
-
   runApp(MaterialApp(
-    home: Container(),
+    home: Home(),
   ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text('\$ Conversor \$'),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: ,
+    );
+  }
 }
