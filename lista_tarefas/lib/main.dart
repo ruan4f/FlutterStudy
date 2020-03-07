33d9f8149999482feca03bb905f3033b01f7bc02
Ruan <ruan4f@gmail.com>
2020-03-07 17:49:00 -0300

    Update main.dart

--- lista_tarefas/lib/main.dart
+++ lista_tarefas/lib/main.dart
@@ -168,6 +168,7 @@ class _HomeState extends State<Home> {
             duration: Duration(seconds: 2),
           );
 
+          Scaffold.of(context).removeCurrentSnackBar();
           Scaffold.of(context).showSnackBar(snack);
         });
       },
