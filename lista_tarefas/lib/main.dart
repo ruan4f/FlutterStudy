fe7804c497ff00d396b26a800bca77ef8dec95b4
Ruan <ruan4f@gmail.com>
2020-03-07 17:48:15 -0300

    Update main.dart

--- lista_tarefas/lib/main.dart
+++ lista_tarefas/lib/main.dart
@@ -52,6 +52,24 @@ class _HomeState extends State<Home> {
     });
   }
 
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+
+    setState(() {
+      _toDoList.sort((a, b) {
+        if (a['ok'] && !b['ok']) {
+          return 1;
+        } else if (!a['ok'] && b['ok']) {
+          return -1;
+        } else {
+          return 0;
+        }
+      });
+    });
+
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -85,10 +103,13 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            child: ListView.builder(
-              padding: EdgeInsets.only(top: 10.0),
-              itemCount: _toDoList.length,
-              itemBuilder: buildItem,
+            child: RefreshIndicator(
+              onRefresh: _refresh,
+              child: ListView.builder(
+                padding: EdgeInsets.only(top: 10.0),
+                itemCount: _toDoList.length,
+                itemBuilder: buildItem,
+              ),
             ),
           )
         ],
