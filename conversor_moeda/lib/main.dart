d320213914828b3a06b48db686694ba74b2c9ada
Ruan <ruan4f@gmail.com>
2020-03-07 13:01:35 -0300

    Update main.dart

--- conversor_moeda/lib/main.dart
+++ conversor_moeda/lib/main.dart
@@ -40,6 +40,11 @@ class _HomeState extends State<Home> {
   double euro;
 
   void _realChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+
     double real = double.parse(text);
 
     dolarController.text = (real / dolar).toStringAsFixed(2);
@@ -47,6 +52,11 @@ class _HomeState extends State<Home> {
   }
 
   void _dolarChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+
     double dolar = double.parse(text);
 
     realController.text = (dolar * this.dolar).toStringAsFixed(2);
@@ -54,10 +64,21 @@ class _HomeState extends State<Home> {
   }
 
   void _euroChanged(String text) {
+    if (text.isEmpty) {
+      _clearAll();
+      return;
+    }
+    
     double euro = double.parse(text);
 
     realController.text = (euro * this.euro).toStringAsFixed(2);
     dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
+  }
+
+  void _clearAll() {
+    realController.text = "";
+    dolarController.text = "";
+    euroController.text = "";
   }
 
   @override
